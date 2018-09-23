ef398f4486dce9977d907e51ebf8c182fd8f073e
John Huynh <john.huynh@student.qut.edu.au>
2018-09-23 19:34:25 +1000

    react app build

--- server.js
+++ server.js
@@ -7,6 +7,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", messages);
+
+process.env.NODE_ENV = "production";
+if (process.env.NODE_ENV === "production") {
+	//render react on the root directory
+	app.use(express.static("client/build"));
+	app.get("*", (req, res) => {
+		//Send react build
+		res.sendFile(
+			path.join(path.resolve(__dirname, "client", "build", "index.html"))
+		);
+	});
+}
 // app.use("users", users);
 
 app.listen("5000", () => console.log("Server started on port 5000"));
